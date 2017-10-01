9fda406278c61a74743681c323e4753ad6e2b29a
Kadir Pekel <kadirpekel@gmail.com>
2017-10-02 02:48:43 +0300

    meta enhancement

--- src/main/java/com/ratemarkt/models/Result.java
+++ src/main/java/com/ratemarkt/models/Result.java
@@ -6,9 +6,16 @@ import javax.annotation.Nullable;
 
 public interface Result<T extends Query> {
 
+	public static final String META_SUPPLIER_REF = "supplierRef";
+	
+	
 	@Nullable
 	T getQuery();
 
 	@Nullable
 	Map<String, Object> getDebugData();
+
+	@Nullable
+	Map<String, Object> getMeta();
+
 }
